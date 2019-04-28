e768a0eb6a5c5bbe43f44a1df876b7528dc99d9e
Wechuli <wechulipaul@yahoo.com>
2019-04-28 18:16:20 +0300

    ass and operators

---
+++ js-guide/expressions-operators/assignment.js
@@ -0,0 +1,9 @@
+//Assigns a value to its left operand based on the value of its right operand
+
+//destructuring
+
+const foo = ["one", "two", "three"];
+
+let [one, two, three] = foo;
+
+console.log(two);
---
+++ js-guide/expressions-operators/comparison-and-arithm.js
@@ -0,0 +1,2 @@
+//A comparison operator compares its operands and returns a logical value based on whether the compariosn is true
+
