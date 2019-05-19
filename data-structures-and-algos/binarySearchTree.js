9dd563990a569767dc24ba2adcc040636d86ba22
Wechuli <wechulipaul@yahoo.com>
2019-05-19 10:21:48 +0300

    testing the binary search tree

--- data-structures-and-algos/binarySearchTree.js
+++ data-structures-and-algos/binarySearchTree.js
@@ -135,6 +135,24 @@ class BST {
         return node;
       }
     };
-    this.root = removeEventListener(this.root, data);
+    this.root = removeNode(this.root, data);
   }
 }
+
+const bst = new BST();
+
+bst.add(4);
+bst.add(2);
+bst.add(6);
+bst.add(1);
+bst.add(3);
+bst.add(5);
+bst.add(7);
+bst.remove(4);
+console.log(bst.findMin())
+console.log(bst.findMax())
+console.log(bst.findMax())
+console.log(bst.isPresent(4))
+
+
+
