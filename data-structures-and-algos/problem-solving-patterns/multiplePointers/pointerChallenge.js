1f3b47bd457b34ee1c165e2b6c45e6c225243e29
Wechuli <wechulipaul@yahoo.com>
2019-07-08 18:15:44 +0300

    sliding window, divide and conquer

--- data-structures-and-algos/problem-solving-patterns/multiplePointers/pointerChallenge.js
+++ data-structures-and-algos/problem-solving-patterns/multiplePointers/pointerChallenge.js
@@ -14,6 +14,8 @@ function countUniqueValues(array) {
   return count;
 }
 
+
+
 console.log(countUniqueValues([1, 1, 1, 1, 1, 2]));
 console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
 console.log(countUniqueValues([-2, -1, -1, 0, 1]));
